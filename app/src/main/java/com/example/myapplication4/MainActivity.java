4919c9c32e393afbbf40635e549be5216aa29655
Admin <awadasel@gmail.com>
2024-09-18 08:51:46 +0300

    eiurherh

--- app/src/main/java/com/example/myapplication4/MainActivity.java
+++ app/src/main/java/com/example/myapplication4/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication4;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+Button stop, start;
+TextView nums ;
+int x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        stop=findViewById(R.id.stop);
+        start=findViewById(R.id.start);
+        nums=findViewById(R.id.textView);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              x=  Integer.parseInt(nums.getText().toString());
+              while (sop)
+            }
         });
+
     }
 }
